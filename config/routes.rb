c05a4075fee07c31a8f6f1315fbe09ba606f2cb1
Edwin Fernandez <fernandez.edwin86@protonmail.com>
2022-10-13 12:47:37 -0400

    Fixed Routing errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get '/home' => 'static_pages#home'
+  get '/help' => 'static_pages#help'
+  get '/about' => 'static_pages#about'
 
-
+  root 'application#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
