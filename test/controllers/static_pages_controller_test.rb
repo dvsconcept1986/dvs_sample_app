e798f81db9a96ec668f3154934f82ed315e685ff
Edwin Fernandez <fernandez.edwin86@protonmail.com>
2022-11-01 12:02:24 -0400

    Created title helper. Verified all testes passed.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,26 +1,23 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-def setup
-  @base_title = "DvsSampleApp"
-end
 
   test "should get home" do
     get home_url 
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Home | DvsSampleApp"
   end
 
   test "should get help" do
     get help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | DvsSampleApp"
   end
 
   test "should get about" do
     get about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | DvsSampleApp"
   end 
 
 end
