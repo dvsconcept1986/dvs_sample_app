c664b85d20ec5e30e4417f73473ea7affa542bfe
Edwin Fernandez <fernandez.edwin86@protonmail.com>
2022-11-04 13:47:32 -0400

    Added contact page. Created tests for the contact page.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,21 +3,27 @@ require "test_helper"
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
-    get home_url 
+    get home_path 
     assert_response :success
     assert_select "title", "Home | DvsSampleApp"
   end
 
   test "should get help" do
-    get help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | DvsSampleApp"
   end
 
   test "should get about" do
-    get about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | DvsSampleApp"
-  end 
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | DvsSampleApp"
+  end
 
 end
